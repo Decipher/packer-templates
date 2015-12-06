282be60ac063f43e17dc0e826027e8da6ee1f82a
Thom Toogood <thomtoogood@gmail.com>
2015-12-07 01:15:49 +1100

    Updated ansible install for 14.04.

--- ubuntu-14.04-x86_64/scripts/ansible.sh
+++ ubuntu-14.04-x86_64/scripts/ansible.sh
@@ -2,11 +2,7 @@
 
 # Install ansible.
 apt-get -y update
-apt-get -y install python-software-properties
+apt-get -y install software-properties-common
 apt-add-repository -y ppa:ansible/ansible
 apt-get -y update
 apt-get -y install ansible
-
-# Create staging directory.
-mkdir /ansible
-chown -R vagrant:vagrant /ansible
