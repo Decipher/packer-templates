1e8f4b04b8a42df78d56f3c420c2903c2fc24c97
Thom Toogood <thomtoogood@gmail.com>
2015-12-07 01:20:29 +1100

    Updated to use DrupalMel git repo.

--- ubuntu-14.04-x86_64/scripts/beetbox.sh
+++ ubuntu-14.04-x86_64/scripts/beetbox.sh
@@ -8,7 +8,7 @@ BEETBOX_HOME="/beetbox"
 
 # Checkout beetbox.
 mkdir -p $BEETBOX_HOME
-git clone https://github.com/thom8/beetbox.git $BEETBOX_HOME
+git clone https://github.com/drupalmel/beetbox.git $BEETBOX_HOME
 
 # Provision box.
 chmod +x $BEETBOX_HOME/ansible/build.sh
