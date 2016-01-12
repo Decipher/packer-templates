2d9c0b7363dc52d7168bc2d7dde1cac64a6ca60b
Stuart Clark <stu@rtclark.net>
2016-01-13 09:50:46 +1100

    Changed Ansible version to 1.9.4

--- ubuntu-12.04-x86_64/scripts/ansible.sh
+++ ubuntu-12.04-x86_64/scripts/ansible.sh
@@ -5,7 +5,7 @@ apt-get -y update
 apt-get -y install python-software-properties
 apt-add-repository -y ppa:ansible/ansible
 apt-get -y update
-apt-get -y install ansible
+apt-get -y install ansible=1.9.4-1ppa~precise
 
 # Create staging directory.
 mkdir /ansible
